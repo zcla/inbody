01d853d51336abc584c4d603fb7c3b1e15e9b005
zcla <zeclaudioaguiar@gmail.com>
2025-05-16 18:15:18 -0300

    Spring initializr

---
+++ src/main/java/zcla71/inbody/InBodyApplication.java
@@ -0,0 +1,13 @@
+package zcla71.inbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InBodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InBodyApplication.class, args);
+	}
+
+}
