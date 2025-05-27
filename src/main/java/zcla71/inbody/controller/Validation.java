31cf53e49b5b89d328b603e3847132c8c864f9db
zcla <zeclaudioaguiar@gmail.com>
2025-05-27 02:53:09 +0000

    Decora campos só se houve validação

--- src/main/java/zcla71/inbody/controller/Validation.java
+++ src/main/java/zcla71/inbody/controller/Validation.java
@@ -17,6 +17,9 @@ public class Validation {
 	}
 
 	public static final String validationClass(List<Validation> validations, String fieldName) {
+		if (validations == null) {
+			return "";
+		}
 		if (isValid(validations, fieldName)) {
 			return "is-valid";
 		}
